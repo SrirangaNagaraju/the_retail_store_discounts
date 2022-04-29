d21a1f302d0c64056753f2bc88a5ea527a2591bb
SrirangaNagaraju <104584259+SrirangaNagaraju@users.noreply.github.com>
2022-04-29 11:39:04 +0530

    Delete Products.java
    mismatch package

--- src/main/java/com/nagaraju/retail_store/Products.java
+++
@@ -1,9 +0,0 @@
-package com.nagaraju.retail_store.model;
-
-public interface Products {
-
-    String getProductName();
-    ProductType getProductType();
-    double getProductUnitPrice();
-    double getTotalPrice(int quantity);
-}
