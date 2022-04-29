34739556176a2708d2097398411d39018ba8aafd
SrirangaNagaraju <104584259+SrirangaNagaraju@users.noreply.github.com>
2022-04-29 12:05:55 +0530

    Update Cart.java
    made it correct model package name.

--- src/main/java/com/nagaraju/retail_store/service/Cart.java
+++ src/main/java/com/nagaraju/retail_store/service/Cart.java
@@ -1,7 +1,7 @@
 package com.nagaraju.retail_store.service;
 
-import com.nagraju.retail_store.model.Products;
-import com.nagraju.retail_store.model.User;
+import com.nagaraju.retail_store.model.Products;
+import com.nagaraju.retail_store.model.User;
 
 public interface Cart {
     void addProducts(Products products, int quantity);
