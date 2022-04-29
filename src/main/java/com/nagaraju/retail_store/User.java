268588a9c76e8fd0093efe400392afb2dad01964
SrirangaNagaraju <104584259+SrirangaNagaraju@users.noreply.github.com>
2022-04-29 11:36:04 +0530

    Add files via upload
    Added model classes correct package

---
+++ src/main/java/com/nagaraju/retail_store/User.java
@@ -0,0 +1,15 @@
+package com.nagaraju.retail_store.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private String userName;
+    private String mobileNumber;
+    private LocalDateTime registeredDateAndTime;
+    private LocalDateTime purchaseDateAndTime;
+    private UserType userType;
+}
