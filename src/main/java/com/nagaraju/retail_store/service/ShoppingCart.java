9187952ef979b8089e142595ad4e4937d3f1d474
SrirangaNagaraju <104584259+SrirangaNagaraju@users.noreply.github.com>
2022-04-29 11:04:40 +0530

    Add files via upload
    code changed it.

---
+++ src/main/java/com/nagaraju/retail_store/service/ShoppingCart.java
@@ -0,0 +1,96 @@
+package com.nagaraju.retail_store.service;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+import com.nagraju.retail_store.model.Discounts;
+import com.nagraju.retail_store.model.ProductType;
+import com.nagraju.retail_store.model.Products;
+import com.nagraju.retail_store.model.User;
+import com.nagraju.retail_store.model.UserType;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+@ComponentScan(basePackages = { "com.nagaraju.retail_store.constant" })
+public class ShoppingCart implements Cart {
+
+	private static final Logger logger = LoggerFactory.getLogger(StoreService.class);
+
+	@Getter
+	@Setter
+	private Map<Products, Integer> productQuantityMap;
+	private Discounts discounts;
+	private User user;
+
+	public ShoppingCart(Discounts discounts, User user) {
+		productQuantityMap = new LinkedHashMap<>();
+		this.discounts = discounts;
+		this.user = user;
+	}
+
+	@Override
+	public void addProducts(Products products, int quantity) {
+		logger.debug("ShoppingCart inside addProducts(products, quantity) method...");
+		int storedQuantity = productQuantityMap.getOrDefault(products, 0);
+		productQuantityMap.put(products, storedQuantity + quantity);
+	}
+
+	@Override
+	public int checkUserTypeAndGetDiscounts(User user) {
+		logger.debug("ShoppingCart inside checkUserTypeAndGetDiscounts(User user) method...");
+		int returnValue;
+
+		switch (user.getUserType()) {
+		case EMPLOYEE:
+			returnValue = 30;
+			break;
+		case AFFILIATE:
+			returnValue = 10;
+			break;
+		case GENERAL:
+			if (ChronoUnit.YEARS.between(user.getRegisteredDateAndTime(), LocalDateTime.now()) >= 2) {
+				returnValue = 5;
+				break;
+			}
+		default:
+			returnValue = 0;
+			break;
+		}
+
+		return returnValue;
+	}
+
+	@Override
+	public double totalAmount() {
+		logger.debug("ShoppingCart inside totalAmount() method...");
+		double total = 0;
+		for (Products product : productQuantityMap.keySet()) {
+			double productPrice = product.getTotalPrice(productQuantityMap.get(product));
+			if (product.getProductType() != ProductType.GROCERY) {
+				total += (productPrice - (productPrice * checkUserTypeAndGetDiscounts(user) / 100));
+			} else {
+				total += productPrice;
+			}
+
+		}
+
+		if (discounts != null)
+			total = discounts.applyDiscount(total);
+
+		return total;
+	}
+}
