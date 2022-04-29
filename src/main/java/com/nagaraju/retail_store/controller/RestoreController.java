8baaf81b3564a4525342a42ccbb08c0dc85943d6
SrirangaNagaraju <104584259+SrirangaNagaraju@users.noreply.github.com>
2022-04-29 12:04:26 +0530

    Update RestoreController.java
    model package name correct it.

--- src/main/java/com/nagaraju/retail_store/controller/RestoreController.java
+++ src/main/java/com/nagaraju/retail_store/controller/RestoreController.java
@@ -13,13 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.nagaraju.retail_store.service.Cart;
 import com.nagaraju.retail_store.service.ShoppingCart;
 import com.nagaraju.retail_store.service.StoreService;
-import com.nagraju.retail_store.model.Discounts;
-import com.nagraju.retail_store.model.Product;
-import com.nagraju.retail_store.model.ProductType;
-import com.nagraju.retail_store.model.Products;
-import com.nagraju.retail_store.model.StoreDiscounts;
-import com.nagraju.retail_store.model.User;
-import com.nagraju.retail_store.model.UserType;
+import com.nagaraju.retail_store.model.Discounts;
+import com.nagaraju.retail_store.model.Product;
+import com.nagaraju.retail_store.model.ProductType;
+import com.nagaraju.retail_store.model.Products;
+import com.nagaraju.retail_store.model.StoreDiscounts;
+import com.nagaraju.retail_store.model.User;
+import com.nagaraju.retail_store.model.UserType;
 
 @Component
 @RestController
